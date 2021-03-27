08eefb2d5fd3dce40cf0b35e5af1c13684cc65aa
CrispyPin <crispin@tasa.se>
2021-03-27 22:41:47 +0100

    some optimisations and cleanup

--- game-of-life.js
+++ game-of-life.js
@@ -4,45 +4,41 @@
 const nx = [-1, 0, 1, -1, 1, -1, 0, 1];
 const ny = [-1, -1, -1, 0, 0, 1, 1, 1];
 
+const cellSize = 6;
+const margin = 1;
+const framesPerStep = 5;
+
 class GameOfLife {
-    constructor(id, cellSize=16, timeStep=10) {
-        this.app = document.getElementById(id);
-        this.canvas = this.app.getElementsByTagName("canvas")[0];
+    constructor(id) {
+        this.canvas = document.getElementById(id);
         this.ctx = this.canvas.getContext("2d");
 
-        var width = Math.floor(window.innerWidth / cellSize);
-        //var height = Math.floor(1.2 * this.canvas.clientHeight / cellSize);
-        var height =  Math.floor(window.innerHeight / cellSize);
+        this.width = Math.floor(window.innerWidth / cellSize);
+        this.height =  Math.floor(window.innerHeight / cellSize);
 
-        this.canvas.width = width * cellSize;
-        this.canvas.height = height * cellSize;
+        this.canvas.width = this.width * cellSize;
+        this.canvas.height = this.height * cellSize;
 
         this.run = this.run.bind(this);
         
-        this.worldWidth = width;
-        this.worldHeight = height;
-        this.world = matrix(width, height, "r");
-        this.cellSize = cellSize;
-
-        this.timeStep = timeStep;//frames per step
-        this.sinceStep = 0;
+        this.world = matrix(this.width, this.height, "r");
         
-        this.run();
+        this.sinceStep = 0;
     }
 
     step() {
-        let newWorld = matrix(this.worldWidth, this.worldHeight);
-        let neighbors = matrix(this.worldWidth, this.worldHeight, 0);
+        let newWorld = matrix(this.width, this.height);
+        let neighbors = matrix(this.width, this.height, 0);
         
         //count neighbors
-        for (let y = 0; y < this.worldHeight; y++) {
-            for (let x = 0; x < this.worldWidth; x++) {
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
                 if (this.world[y][x]) {
                     //add to neighbor count of all surrounding cells
                     for (let pos = 0; pos < 8; pos++) {
                         let xp = x+nx[pos];
                         let yp = y+ny[pos];
-                        if (xp < this.worldWidth && xp >= 0 && yp < this.worldHeight && yp >= 0) {
+                        if (xp < this.width && xp >= 0 && yp < this.height && yp >= 0) {
                             neighbors[yp][xp]++;
                         }
                     }
@@ -51,33 +47,28 @@ class GameOfLife {
         }
     
         //update state
-        for (let y = 0; y < this.worldHeight; y++) {
-            for (let x = 0; x < this.worldWidth; x++) {
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
                 newWorld[y][x] = this.newState(this.world[y][x], neighbors[y][x]);
             }
         }
-        
         this.world = newWorld;
-        
-        this.render();
     }
 
     newState(state, neighbors) {
         if (state) {
-            return (neighbors >= 2 && neighbors <= 3);
-        } else {
-            return neighbors == 3;
+            return (neighbors > 1 && neighbors < 4);
         }
+        return neighbors == 3;
     }
     
     render() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.fillStyle = "#048";
-        for (let y = 0; y < this.worldHeight; y++) {
-            for (let x = 0; x < this.worldWidth; x++) {
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
                 if (this.world[y][x]) {
-                    this.ctx.fillStyle = "rgb("+x/this.worldWidth*255 + "," + y/this.worldHeight*255 +",255)";
-                    this.ctx.fillRect(x * this.cellSize, y * this.cellSize, this.cellSize - 1, this.cellSize - 1);
+                    this.ctx.fillStyle = "rgb("+(x/this.width*192+64) + "," + (y/this.height*192+64) +",192)";
+                    this.ctx.fillRect(x * cellSize, y * cellSize, cellSize - margin, cellSize - margin);
                 }
             }
         }
@@ -85,21 +76,16 @@ class GameOfLife {
 
     run() {
         this.sinceStep++;
-        if (this.sinceStep >= this.timeStep) {
+        if (this.sinceStep >= framesPerStep) {
             this.sinceStep = 0;
             this.step();
+            this.render();
         }
         window.requestAnimationFrame(this.run);
-        
-    }
-
-    randomizeWorld() {
-        this.world = matrix(this.worldWidth, this.worldHeight, "r");
-        this.render();
     }
 }
 
-function matrix(width, height, fill=false, weight=0.3) {
+function matrix(width, height, fill=false, weight=0.4) {
     let m = [];
     let content = fill;
 
@@ -116,4 +102,7 @@ function matrix(width, height, fill=false, weight=0.3) {
     return m;
 }
 
-let gameOfLife = new GameOfLife("gol", 6, 5);
+let gameOfLife = new GameOfLife("gol");
+
+gameOfLife.run();
+
